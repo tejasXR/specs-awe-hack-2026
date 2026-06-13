4496532bb49113cfe959143cdd9761e2ceed524b
Tejas Shroff <tejasshroff24@gmail.com>
2026-06-13 13:57:43 -0400

    Menu console improvements

--- Project/Assets/_App/Scripts/UI/MenuConsole.ts
+++ Project/Assets/_App/Scripts/UI/MenuConsole.ts
@@ -1,8 +1,14 @@
+import { LSTween } from "LSTween.lspkg/Examples/Scripts/LSTween";
+import Easing from "LSTween.lspkg/TweenJS/Easing";
+import { Tween } from "LSTween.lspkg/TweenJS/Tween";
 import { Interactable } from "SpectaclesInteractionKit.lspkg/Components/Interaction/Interactable/Interactable";
 import Event, { PublicApi } from "SpectaclesInteractionKit.lspkg/Utils/Event";
 
 @component
 export class MenuConsole extends BaseScriptComponent {
+  @input
+  container!: SceneObject;
+
   @input
   primaryButton!: Interactable;
 
@@ -24,6 +30,9 @@ export class MenuConsole extends BaseScriptComponent {
   readonly onTertiaryPressed: PublicApi<void> =
     this.onTertiaryPressedEvent.publicApi();
 
+  private showDurationMs: number = 250;
+  private _activeTween?: Tween<{ t: number }>;
+
   onAwake() {
     this.createEvent("OnStartEvent").bind(() => this.onStart());
   }
@@ -40,6 +49,30 @@ export class MenuConsole extends BaseScriptComponent {
     );
   }
 
-  public show(): void {}
-  public hide(): void {}
+  public show(): void {
+    this.stopActiveTween();
+    this.container.enabled = true;
+
+    const transform = this.container.getTransform();
+    transform.setLocalScale(vec3.zero());
+
+    this._activeTween = LSTween.scaleToLocal(
+      transform,
+      vec3.one(),
+      this.showDurationMs,
+    )
+      .easing(Easing.Back.Out)
+      .start();
+  }
+
+  public hide(): void {
+    this.stopActiveTween();
+    this.container.getTransform().setLocalScale(vec3.zero());
+    this.container.enabled = false;
+  }
+
+  private stopActiveTween(): void {
+    this._activeTween?.stop();
+    this._activeTween = undefined;
+  }
 }
